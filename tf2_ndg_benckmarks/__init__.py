b9b3921cb60f84b1cb2fdb277f556c17e6708629
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-11-11 12:54:11 +0900

    Add documentation module and docs

---
+++ tf2_ndg_benckmarks/__init__.py
@@ -0,0 +1,12 @@
+"""
+    tf2-ndg-benchmarks
+
+    Neural Dialogue Generation Benchmarks implemented TensorFlow 2.0.
+
+    Copyright:
+        Copyright 2019 by Katsuya SHIMABUKURO.
+    License:
+        MIT, see LICENSE for details.
+"""
+
+__version__ = '0.1.0'
