38c33305a8115475e46edf433df1d5b4ce9607ec
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-11-16 22:32:41 +0900

    Add tesst case BLEU-2

--- tests/test_bleu.py
+++ tests/test_bleu.py
@@ -17,3 +17,11 @@ class TestBleu:
 
         expect = nltk.translate.bleu_score.sentence_bleu([reference.split(' ')], hypothesis.split(' '))
         eq_(expect, scorer.sentence_score(reference, hypothesis))
+
+    def test_sentence_score_bleu_2(self):
+        reference = 'It is a guide to action that ensures that the military will forever heed Party commands'
+        hypothesis = 'It is a guide to action which ensures that the military always obeys the commands of the party'
+        scorer = bleu.Bleu()
+
+        expect = nltk.translate.bleu_score.sentence_bleu([reference.split(' ')], hypothesis.split(' '), weights=(0.5, 0.5))
+        eq_(expect, scorer.sentence_score(reference, hypothesis, n=2))
