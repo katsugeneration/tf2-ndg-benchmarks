dfd2175c4ee043dee59f67a4b991b0579ce6d82c
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-11-16 22:12:12 +0900

    Implment sentence_score function

--- tf2_ndg_benckmarks/metrics/bleu.py
+++ tf2_ndg_benckmarks/metrics/bleu.py
@@ -1,6 +1,34 @@
+"""
+Copyright:
+    Copyright 2019 by Katsuya SHIMABUKURO.
+License:
+    MIT, see LICENSE for details.
+"""
+from collections import Counter
+import numpy as np
+
 
 class Bleu(object):
     """BLEU score calculator."""
+
+    def _count_ngram(
+            self,
+            n: int,
+            target: str) -> Counter:
+        """Count n-gram.
+
+        Args:
+            n (int): n-gram's n.
+            target: (str): target sentence.
+
+        Return:
+            Counter: python counter object.
+
+        """
+        tokens = target.split(' ')
+        N = len(tokens)
+        counter = Counter(map(lambda x: ' '.join(x), zip(*[tokens[i:N-n+i+1] for i in range(n)])))
+        return counter
 
     def sentence_score(
             self,
@@ -16,4 +44,14 @@ class Bleu(object):
             float: Sentence BLEU score
 
         """
-        return 0.0
+        N = 4
+        precision = np.exp(sum(
+            [1./float(N) * np.log(
+                len(set(self._count_ngram(n, hypothesis).keys()) & set(self._count_ngram(n, reference).keys())) /
+                sum(self._count_ngram(n, hypothesis).values()))
+                for n in range(1, N+1)]))
+
+        c = float(len(hypothesis.split(' ')))
+        r = float(abs(len(reference.split(' '))))
+        BP = 1 if c > r else np.exp(1 - r / c)
+        return BP * precision
