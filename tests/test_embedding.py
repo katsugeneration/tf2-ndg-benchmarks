60e7ed84124b041a01ee57ab1ceecc56ebe0a75c
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-11-30 23:34:10 +0900

    Add embedding average score test

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -4,7 +4,7 @@ Copyright:
 License:
     MIT, see LICENSE for details.
 """
-from nose.tools import ok_
+from nose.tools import ok_, eq_
 import os
 from tf2_ndg_benckmarks.metrics import embedding
 
@@ -14,3 +14,13 @@ class TestBase:
         base = embedding.EmbeddingBase()
         ok_(os.path.exists(base.emb_path))
         ok_(base.model)
+
+
+class TestAverage:
+    def test_sentence_score(self):
+        reference = 'It is a guide to action that ensures that the military will forever heed Party commands'
+        hypothesis = 'It is a guide to action which ensures that the military always obeys the commands of the party'
+        scorer = embedding.Average()
+
+        eq_(1.0, scorer.sentence_score(reference, reference))
+        ok_(scorer.sentence_score(reference, hypothesis) > 0.0)
