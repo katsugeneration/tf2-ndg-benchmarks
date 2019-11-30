bf9293d7fe315d0c5d5718c5dd6e1c72fef8cbc1
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-12-01 00:12:56 +0900

    Add VectorExtrema score calc test

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -24,3 +24,13 @@ class TestAverage:
 
         ok_(scorer.sentence_score(reference, reference) - 1.0 <= 0.000001)
         ok_(scorer.sentence_score(reference, hypothesis) > 0.0)
+
+
+class TestVectorExtrema:
+    def test_sentence_score(self):
+        reference = 'It is a guide to action that ensures that the military will forever heed Party commands'
+        hypothesis = 'It is a guide to action which ensures that the military always obeys the commands of the party'
+        scorer = embedding.VectorExtrema()
+
+        ok_(scorer.sentence_score(reference, reference) - 1.0 <= 0.000001)
+        ok_(scorer.sentence_score(reference, hypothesis) > 0.0)
