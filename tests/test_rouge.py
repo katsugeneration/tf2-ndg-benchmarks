d2ae45e9af29e8ff8928093ce4e6b95edfa8feeb
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-11-30 18:18:13 +0900

    Add ROUGE-1 test

---
+++ tests/test_rouge.py
@@ -0,0 +1,20 @@
+"""
+Copyright:
+    Copyright 2019 by Katsuya SHIMABUKURO.
+License:
+    MIT, see LICENSE for details.
+"""
+from nose.tools import eq_
+from sumeval.metrics.rouge import RougeCalculator
+from tf2_ndg_benckmarks.metrics import rouge
+
+
+class TestRouge:
+    def test_sentence_score(self):
+        reference = 'I went to Mars'
+        hypothesis = 'I went to the Mars from my living town.'
+        scorer = rouge.Rouge()
+
+        rouge_calc = RougeCalculator(stopwords=False, lang="en")
+        expect = rouge_calc.rouge_n(hypothesis, reference, n=1)
+        eq_(expect, scorer.sentence_score(reference, hypothesis))
