8a39cf48890a90b8c28b4eae8ad9aa56ad330bf9
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-12-01 11:52:02 +0900

    Add distinct-2 test

--- tests/test_distinct.py
+++ tests/test_distinct.py
@@ -18,3 +18,13 @@ class TestDistinct:
 
         eq_(0.5, scorer.sentence_score([hypothesis3, hypothesis3]))
         ok_(scorer.sentence_score([hypothesis1, hypothesis1]) < scorer.sentence_score([hypothesis1, hypothesis2]))
+
+    def test_sentence_score_distinct_2(self):
+        hypothesis1 = 'It is a guide to action which ensures that the military always obeys the commands of the party'
+        hypothesis2 = 'It is a guide to action that ensures that the military will forever heed Party commands'
+        hypothesis3 = 'It is fine day.'
+        scorer = distinct.Distinct()
+
+        eq_(0.5, scorer.sentence_score([hypothesis3, hypothesis3], n=2))
+        ok_(scorer.sentence_score([hypothesis1, hypothesis1], n=2) < scorer.sentence_score([hypothesis1, hypothesis2], n=2))
+        ok_(scorer.sentence_score([hypothesis1, hypothesis2], n=1) < scorer.sentence_score([hypothesis1, hypothesis2], n=2))
